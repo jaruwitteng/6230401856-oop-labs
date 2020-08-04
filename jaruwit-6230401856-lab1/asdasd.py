1683a856640c3fd5fa7b703d0dd58653e23ed613
jaruwitteng <69178853+jaruwitteng@users.noreply.github.com>
2020-08-04 11:23:07 +0700

    Delete asdasd.py

--- jaruwit-6230401856-lab1/asdasd.py
+++
@@ -1,9 +0,0 @@
-import random
-import statistics
-number1 = (random.randint(1, 10))
-number2 = (random.randint(1, 10))
-average = (number1 + number2)/2
-numbers_store = [number1, number2]
-std = (statistics.stdev(numbers_store))
-print("The average of", number1, "and", number2, "is", average)
-print("The standard deviation of", number1, "and", number2,  "is", std%2)
