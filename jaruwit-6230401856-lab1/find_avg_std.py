db0c3522fa604d6210130cb70ad74eefa44e75fd
jaruwitteng <69178853+jaruwitteng@users.noreply.github.com>
2020-08-04 09:11:23 +0700

    Add files via upload

---
+++ jaruwit-6230401856-lab1/find_avg_std.py
@@ -0,0 +1,8 @@
+import random
+number1 = (random.randint(1, 10))
+number2 = (random.randint(1, 10))
+average = (number1 + number2)/2
+standard = (number1 - number2)/2
+standard = abs(standard)
+print("The average of", number1, "and", number2, "is", average)
+print("The standard deviation of", number1, "and", number2,  "is", standard)
